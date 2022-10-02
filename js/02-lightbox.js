d07cd39140c903c4cd6a9c71a5661dbaf25ebe56
PiotrDrechsler <pdrech@gmail.com>
2022-10-02 20:03:37 +0200

    Update task-02
    task02 - ready to check

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const gallery = document.querySelector("ul.gallery");
+const createGallery = galleryItems // gallery
+  .map(
+    (item) =>
+      `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", createGallery); // Create gallery
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
 console.log(galleryItems);
